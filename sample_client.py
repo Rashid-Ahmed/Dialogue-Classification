4beae74da759447c1a22b79346164b95c7c81c41
Rashid Ahmed <37401217+Rashid-Ahmed@users.noreply.github.com>
2023-03-29 21:08:17 +0200

    Create sample_client.py

---
+++ sample_client.py
@@ -0,0 +1,25 @@
+import socket
+import json
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+host = config.get('DEFAULT', 'IP')
+PORT = config.get('DEFAULT', 'PORT')
+
+client_socket = socket.socket()  # instantiate
+client_socket.connect((host, port))  # connect to the server
+json_data = ''
+while True:
+    sentence = "something (you get this from the user)"
+    client_socket.send(sentence.encode())  # send message
+    #if the user chat is over you set sentence to "connection closed"
+    if sentence == "connection closed":
+        break
+    data = client_socket.recv(64000).decode()  # receive response
+    data = json.loads(data)
+    json_data = data
+    print (data)
+
+client_socket.close()  # close the connection
